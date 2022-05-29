7ceaf14c527fda29314204514da95f5d272d9d7e
baichaun <woshibaichuan@163.com>
2022-05-29 18:16:16 +0800

    Init

---
+++ Assets/Script/SwitchNeer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchNeer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        var parent = transform.parent;
+        for (int i = 0; i < transform.parent.childCount; i++)
+        {
+            parent.GetChild(i).gameObject.SetActive(parent.GetChild(i)==transform);
+        }
+    }
+}
